a6fa19f82390b208a674666d2c32f49d0e079fbb
Guy <gby78il@gmail.com>
2026-05-18 23:47:02 +0300

    Update: change button to snow effect

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("Streamlit Button Example")
+st.title("האפליקציה שלי ב-NAS 🚀")
+st.write("הגרסה המעודכנת ביותר שלי!")
 
-if st.button("Click Me"):
-    st.write("Button clicked!")
+if st.button("לחץ כאן להפתעה חדשה"):
+    st.snow() # יציג אפקט של שלג/קונפטי במקום בלונים
+    st.success("האוטומציה עובדת בהצלחה!")
